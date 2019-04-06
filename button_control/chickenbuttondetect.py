3fa256752b09ccd9f7f142e498cbe4e9d2b9afc1
Koen <koenvaes+git@gmail.com>
2019-04-06 21:28:56 +0200

    Corrected typo's and errors

--- button_control/chickenbuttondetect.py
+++ button_control/chickenbuttondetect.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# noinspection SpellCheckingInspection
 """
 ================================================
 Uses ABElectronics IO Pi 32-Channel Port Expander
@@ -10,19 +11,19 @@ For Python 3 install with: sudo apt-get install python3-smbus
 
 ================================================
 
-This example will control the door using simple button press and will detect either of the 
+This example will control the door using simple button press and will detect either of the
 two end states using a switch
 
 Initialises the IOPi device using the default address
 for Bus 2, you will need to change the addresses if you have changed
 the jumpers on the IO Pi
 """
+from __future__ import absolute_import, division, print_function, \
+    unicode_literals
+
+import sys
 import time
 
-from __future__ import absolute_import, division, print_function, \
-                                                    unicode_literals
-			
-			
 try:
     from IOPi import IOPi
 except ImportError:
@@ -35,148 +36,147 @@ except ImportError:
         raise ImportError(
             "Failed to import library from parent folder")
 
-			
+
 class Hardware:
     """
     Hardware abstraction Class
     """
-	    bus = None
-		
+    bus = None
 
-    def __init__(self,simulated):
+    def __init__(self, simulated):
         """
         __init__ is called at startup
         """
 
-		self.simulatedHw = simulated
-		
-		if self.simulatedHw :
-			self.doorStateUp = False
-		else:
-			# create an instance of Bus 1 which is on I2C address 0x21 by default
-			self.bus = IOPi(0x20)
+        self.simulatedHw = simulated
 
-			# set pins 1 to 8 to be outputs and turn them off
-			self.bus.set_port_direction(0, 0x00)
-			self.bus.write_port(0, 0x00)
+        if self.simulatedHw:
+            self.doorStateUp = False
+        else:
+            # create an instance of Bus 1 which is on I2C address 0x21 by default
+            self.bus = IOPi(0x20)
 
-			# set pins 9 to 16 to be inputs and turn pullup on
-			self.bus.set_port_direction(1, 1)
-			self.bus.set_port_pullups(1, 1)
-			self.timeToMove = 20
-			if detect_door_down():
-				self.doorStateUp = False
-			else if 
-				if detect_door_up():
-					self.doorStateUp = True
-					else #reboot while undetermined state -> close door
-						self.doorStateUp = True
-						motor_down()
-					
+            # set pins 1 to 8 to be outputs and turn them off
+            self.bus.set_port_direction(0, 0x00)
+            self.bus.write_port(0, 0x00)
 
-	def motor_stop (self):
-	"""
-	controls the H-bridge to stop the motor.
-	"""
-		if self.simulatedHw == False :
-			#set all H-bridge switches to off
-			self.bus.write_pin(1, 0x00)
-			self.bus.write_pin(2, 0x00)
-			self.bus.write_pin(3, 0x00)
-			self.bus.write_pin(4, 0x00)
+            # set pins 9 to 16 to be inputs and turn pull-up on
+            self.bus.set_port_direction(1, 1)
+            self.bus.set_port_pullups(1, 1)
+            self.timeToMove = 20
+            if self.detect_door_down():
+                self.doorStateUp = False
+            elif self.detect_door_up():
+                self.doorStateUp = True
+            else:
+                # reboot while undetermined state -> close door
+                self.doorStateUp = True
+                self.motor_down()
 
-	def motor_up (self):
-	"""
-	controls the H-bridge to move the motor in a specific direction.
-	"""
-		if self.simulatedHw == False :
-			#avoid conflicts
-			motor_stop()
-			
-			#enable up
-			self.bus.write_pin(1, 0x01)
-			self.bus.write_pin(3, 0x01)
+    def motor_stop(self):
+        """
+        controls the H-bridge to stop the motor.
+        """
+        if not self.simulatedHw:
+            # set all H-bridge switches to off
+            self.bus.write_pin(1, 0x00)
+            self.bus.write_pin(2, 0x00)
+            self.bus.write_pin(3, 0x00)
+            self.bus.write_pin(4, 0x00)
 
-			time.sleep(self.timeToMove)
-			motor_stop()
-		self.doorStateUp = True
-		
-	def motor_down (self):
-	"""
-	controls the H-bridge to move the motor in a specific direction.
-	"""
-		if self.simulatedHw == False :
-			#avoid conflicts
-			motor_stop()
-					
-			#enable downward sidecof H-bridge
-			self.bus.write_pin(1, 0x01)
-			self.bus.write_pin(2, 0x01)
-			
-			time.sleep(self.timeToMove)
-			motor_stop()
-		self.doorStateUp = False
-		
-	def button_pressed (self):
-	"""
-	checks if the button is pressed with debouncing.
-	"""
-		if self.simulatedHw == True :
-			return False
-		else:
-			pin = 9
-			if read_pin(pin) = 0 :
-				time.sleep(0.05)
-				if read_pin(pin) = 0 : #debounce 50 ms
-					return True
-			return False	
+    def motor_up(self):
+        """
+        controls the H-bridge to move the motor in a specific direction.
+        """
+        if not self.simulatedHw:
+            # avoid conflicts
+            self.motor_stop()
 
-	def detect_door_down (self):
-	"""
-	checks if the door if completely at the down endstop
-	"""
-		if self.simulatedHw == True :
-			return True
-		else:
-			pin = 10
-			if read_pin(pin) = 0 :
-				time.sleep(0.05)
-				if read_pin(pin) = 0 : #debounce 50 ms
-					return True
-			return False	
-	
-	def detect_door_up (self):
-	"""
-	checks if the door if completely at the down endstop
-	"""
-		if self.simulatedHw == True :
-			return True
-		else:
-			pin = 11
-			if read_pin(pin) = 0 :
-				time.sleep(0.05)
-				if read_pin(pin) = 0 : #debounce 50 ms
-					return True
-			return False	
+            # enable up
+            self.bus.write_pin(1, 0x01)
+            self.bus.write_pin(3, 0x01)
 
-	def is_door_up(self)
-	"""
-	Returns the state of the door without checking the hardware to save detection time 
-	"""
-		return self.doorStateUp
-		
-		
-def main():	
-	poezel = Hardware(False)
-	
-	while True:
-	#move by button press
-		if poezel.button_pressed():
-			if poezel.is_door_up():
-				poezel.motor_down()
-			else 
-				poezel.motor_up()
-	#move by sunup/sundown ( + 30 min)
-	
+            time.sleep(self.timeToMove)
+            self.motor_stop()
+        self.doorStateUp = True
+
+    def motor_down(self):
+        """
+        controls the H-bridge to move the motor in a specific direction.
+        """
+        if not self.simulatedHw:
+            # avoid conflicts
+            self.motor_stop()
+
+            # enable downward side of H-bridge
+            self.bus.write_pin(1, 0x01)
+            self.bus.write_pin(2, 0x01)
+
+            time.sleep(self.timeToMove)
+            self.motor_stop()
+        self.doorStateUp = False
+
+    def button_pressed(self):
+        """
+        checks if the button is pressed with debouncing.
+        """
+        if self.simulatedHw:
+            return False
+        else:
+            pin = 9
+            if self.bus.read_pin(pin) == 0:
+                time.sleep(0.05)
+                if self.bus.read_pin(pin) == 0:  # debounce 50 ms
+                    return True
+            return False
+
+    def detect_door_down(self):
+        """
+        checks if the door if completely at the down end stop
+        """
+        if self.simulatedHw:
+            return True
+        else:
+            pin = 10
+            if self.bus.read_pin(pin) == 0:
+                time.sleep(0.05)
+                if self.bus.read_pin(pin) == 0:  # debounce 50 ms
+                    return True
+            return False
+
+    def detect_door_up(self):
+        """
+        checks if the door if completely at the down end stop
+        """
+        if self.simulatedHw:
+            return True
+        else:
+            pin = 11
+            if self.bus.read_pin(pin) == 0:
+                time.sleep(0.05)
+                if self.bus.read_pin(pin) == 0:  # debounce 50 ms
+                    return True
+            return False
+
+    def is_door_up(self):
+        """
+        Returns the state of the door without checking the hardware to save detection time
+        """
+        return self.doorStateUp
+
+
+def main():
+    io = Hardware(False)
+
+    while True:
+        # move by button press
+        if io.button_pressed():
+            if io.is_door_up():
+                io.motor_down()
+            else:
+                io.motor_up()
+    # move by sunup/sundown ( + 30 min)
+
+
 if __name__ == "__main__":
     main()
